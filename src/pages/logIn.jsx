2cf5da164852bc292a46de20b98c5378b0c3e994
rishubh dixit <rishubhdixit384@gmail.com>
2024-11-27 11:00:03 +0530

    ui changes in post page

--- src/pages/logIn.jsx
+++ src/pages/logIn.jsx
@@ -5,7 +5,6 @@ import { Login as LoginComponent } from '../components';
 function logIn() {
   return (
     <div className="relative py-8 min-h-screen bg-gradient-to-r from-gray-800 via-brown-700 to-black overflow-hidden">
-      {/* Animated Background Elements */}
       <motion.div
         className="absolute top-0 left-0 w-full h-full z-0"
         initial={{ y: '-100%' }}
@@ -22,7 +21,6 @@ function logIn() {
         <div className="absolute bottom-1/4 left-1/4 w-56 h-56 rounded-full bg-purple-500 opacity-20 animate-bounce"></div>
       </motion.div>
 
-      {/* Additional Animated Motion */}
       <motion.div
         className="absolute top-0 left-0 w-full h-full z-0"
         initial={{ opacity: 0 }}
@@ -35,7 +33,6 @@ function logIn() {
         <div className="absolute top-0 left-0 w-full h-full bg-black opacity-30"></div>
       </motion.div>
 
-      {/* Content */}
       <div className="relative z-10">
         <LoginComponent />
       </div>
